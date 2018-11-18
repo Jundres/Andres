123538948a80d2efa3c5c7a9e3a6c31a8d820fda
Jundres <45136824+Jundres@users.noreply.github.com>
2018-11-17 23:24:50 -0600

    Add files via upload

---
+++ OLED3.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>//Comunicacion I2C
+#include <Adafruit_GFX.h>//Libreria GFX
+#include <Adafruit_SSD1306.h>//Libreria SSD1306
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //Direccion hexadecimal del OLED
+  display.clearDisplay(); //Limpia el buffer del display
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  display.clearDisplay();//Limpiamos el buffer de nuevo
+  display.setTextColor(WHITE);//Color del texto
+  display.setCursor(0,0);//Posicion del texto
+  display.setTextSize(1);//Tamaño del texto
+  display.println("Hola, me llamo Andres"); //Texto en pantalla
+  display.println(3.141516);//Texto en pantalla
+  display.display();//Actualizar informacion en el display
+  delay(1000);//Delay de un segundo
+}
